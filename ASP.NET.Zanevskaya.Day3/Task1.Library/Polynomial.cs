7da4b30956612bfc687e2ede6095f52a626810df
Ann Zanevskaya <ann.zanevskaya7@gmail.com>
2015-07-17 11:44:09 +0300

    update methods

--- ASP.NET.Zanevskaya.Day3/Task1.Library/Polynomial.cs
+++ ASP.NET.Zanevskaya.Day3/Task1.Library/Polynomial.cs
@@ -9,6 +9,13 @@ namespace Task1.Library
     public sealed class Polynomial : ICloneable, IEquatable<Polynomial>
     {
         private double[] coefficients;
+        private int minDegree = 0;
+        public int MinDegree {
+            get 
+            {
+                return this.coefficients.Length - 1; 
+            }    
+        }
         public Polynomial()
         {
             coefficients = new double[1];
@@ -18,10 +25,19 @@ namespace Task1.Library
         {
             this.coefficients = coefficients;
         }
+        public double ResultOfPolynomial(double variable)
+        {          
+            double result = 0;
+            for (int i = 0; i < this.coefficients.Length; i++)
+            {
+                result += this[i] * Math.Pow(variable, i);
+            }
+            return result;
+        }
         public static Polynomial operator +(Polynomial firstP, Polynomial secondP)
         {
-            if (firstP == null) throw new ArgumentNullException("firstP");
-            if (secondP == null) throw new ArgumentNullException("secondSP");
+            if ((object)firstP == null) throw new ArgumentNullException("firstP");
+            if ((object)secondP == null) throw new ArgumentNullException("secondSP");
             Polynomial sumP, tempP;
             if (firstP.coefficients.Length > secondP.coefficients.Length)
             {
@@ -41,9 +57,14 @@ namespace Task1.Library
         }
         public static Polynomial operator +(Polynomial firstP, double number)
         {
+            if ((object)firstP == null) throw new ArgumentNullException("firstP");
             Polynomial sum = Copy(firstP);
             sum[0] += number;
             return sum;
+        }
+        public static Polynomial operator +(double number, Polynomial firstP)
+        {
+            return firstP + number;
         }
         public static Polynomial Add(Polynomial firstP, Polynomial secondP)
         {
@@ -53,9 +74,13 @@ namespace Task1.Library
         {
             return firstP + number;
         }
+        public static Polynomial Add(double number, Polynomial firstP)
+        {
+            return firstP + number;
+        }
         public static Polynomial operator -(Polynomial firstP)
         {
-            if (firstP == null) throw new ArgumentNullException("firstP");
+            if ((object)firstP == null) throw new ArgumentNullException("firstP");
             Polynomial negativeP = Copy(firstP);
             for (int i = 0; i < negativeP.coefficients.Length; i++)
             {
@@ -65,14 +90,18 @@ namespace Task1.Library
         }
         public static Polynomial operator -(Polynomial firstP, Polynomial secondP)
         {
-            if (firstP == null) throw new ArgumentNullException("firstP");
-            if (secondP == null) throw new ArgumentNullException("secondSP");
             return firstP + (-secondP);
         }
         public static Polynomial operator -(Polynomial firstP, double number)
         {
             number *= -1;
             return firstP + number;
+        }
+        public static Polynomial operator -(double number, Polynomial firstP)
+        {
+            Polynomial sum = Copy(firstP);
+            sum[0] = -1 *sum[0];
+            return sum + number;
         }
         public static Polynomial Subtract(Polynomial firstP, Polynomial secondP)
         {
@@ -82,8 +111,13 @@ namespace Task1.Library
         {
             return firstP - number;
         }
+        public static Polynomial Subtract(double number, Polynomial firstP)
+        {
+            return number - firstP;
+        }
         public static Polynomial operator *(Polynomial firstP, double number)
         {
+            if ((object)firstP == null) throw new ArgumentNullException("firstP");
             Polynomial sum = Copy(firstP);
             for (int i = 0; i < sum.coefficients.Length; i++)
 			{
@@ -91,17 +125,21 @@ namespace Task1.Library
 			}
             return sum;
         }
+        public static Polynomial operator *(double number, Polynomial firstP)
+        {
+            return firstP * number;
+        }
         public static Polynomial Multiply(Polynomial firstP, double number)
         {
             return firstP * number;
         }
-        public object Clone()
+        public static Polynomial Multiply(double number, Polynomial firstP)
         {
-            return new Polynomial(this.coefficients);
+            return firstP * number;
         }
         public bool Equals(Polynomial other)
         {
-            if (other == null) return false;
+            if ((object)other == null) throw new ArgumentNullException("NULL");
             if (this.coefficients.Length != other.coefficients.Length) return false;
             for (int i = 0; i < this.coefficients.Length; i++)
             {
@@ -109,17 +147,65 @@ namespace Task1.Library
             }
             return true;
         }
+        public static bool operator ==(Polynomial firstP, Polynomial secondP)
+        {    
+            return firstP.Equals(secondP);
+        }
+        public static bool operator ==(Polynomial firstP, double number)
+        {
+            return firstP.Equals(number);
+        }
+        public static bool operator ==(double number, Polynomial firstP)
+        {
+            return firstP.Equals(number);
+        }
+        public static bool operator !=(Polynomial firstP, Polynomial secondP)
+        {
+            return !firstP.Equals(secondP);
+        }
+        public static bool operator !=(Polynomial firstP, double number)
+        {
+            return !firstP.Equals(number);
+        }
+        public static bool operator !=(double number, Polynomial firstP)
+        {
+            return !firstP.Equals(number);
+        }
         public override bool Equals(object obj)
         {
+            if ((object)obj == null) throw new ArgumentNullException("NULL"); ;
             if (coefficients.Length > 1) return false;
             if (obj.GetType() != typeof(int) && obj.GetType() != typeof(double)) return false;
             if (obj.GetType() == typeof(int) && this.coefficients[0] == (int)obj) return true;
             if (obj.GetType() == typeof(double) && this.coefficients[0] == (double)obj) return true;
             return false;
         }
+        public override string ToString()
+        {
+             string result = null;
+             for (int i = 0; i < this.coefficients.Length; i++)
+			{
+                if (this[i] != 0)
+                {
+                    result += +this.coefficients[i] + "x^" + i;
+                    if (i + 1 < this.coefficients.Length) result += "+";
+                }
+			}        
+           return result;
+        }
+
+        //don't sure work's as need :/
         public override int GetHashCode()
         {
             return coefficients.GetHashCode();
+        }
+        public Polynomial Clone()
+        {
+            return new Polynomial(this.coefficients);
+        }
+        object ICloneable.Clone()
+        {
+            return new Polynomial(this.coefficients);
         }
         private static Polynomial Copy(Polynomial firstP)
         {
---
+++ ASP.NET.Zanevskaya.Day3/Task1.Tests/PolynomialTest.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task1.Library;
+
+namespace Task1.Tests
+{
+    [TestClass]
+    public class PolynomialTest
+    {
+        [TestMethod]
+        public void Equality_OfTwoPolynomials()
+        {
+            Polynomial first = new Polynomial(5, 7);
+            Polynomial second = new Polynomial(5, 7);
+
+            Assert.IsTrue(first.Equals(second));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Equality_OfPolynomialAndNULL()
+        {
+            Polynomial first = new Polynomial(5, 7);
+            Polynomial second = null;
+
+            Assert.IsTrue(first.Equals(second));
+        }
+        [TestMethod]
+        public void NonEquality_OfTwoPolynomials()
+        {
+            Polynomial first = new Polynomial(5, 7);
+            Polynomial second = new Polynomial(3, 7, 8);
+
+            Assert.IsFalse(first.Equals(second));
+        }
+        [TestMethod]
+        public void ResultOfPolynomial()
+        {
+            Polynomial first = new Polynomial( 5, 7);         
+            double expected = first.ResultOfPolynomial(2);
+            Assert.AreEqual(19, expected);
+        }    
+        [TestMethod]
+        public void Addition_OfTwoPolynomial()
+        {
+            Polynomial first = new Polynomial(5, 7);
+            Polynomial second = new Polynomial(-5, -7);
+
+            Polynomial result = first + second;
+            Assert.AreEqual(0, result.ResultOfPolynomial(1));
+        }
+         [TestMethod]
+        public void Addition_OfPolynomialAndNumber()
+        {
+            Polynomial first = new Polynomial(5, 7);
+            Polynomial result = -5 + first;
+            Assert.AreEqual(7, result.ResultOfPolynomial(1));
+        }
+         [TestMethod]
+         [ExpectedException(typeof(ArgumentNullException))]
+         public void Addition_OfPolynomialAndNUll()
+         {
+             Polynomial first = new Polynomial(5, 7);
+             Polynomial result = null + first;
+             Assert.AreEqual(7, result.ResultOfPolynomial(1));
+         }
+         [TestMethod]
+         public void Reduce_OfTwoPolynomial()
+         {
+             Polynomial first = new Polynomial(5, 7);
+             Polynomial second = new Polynomial(5, 7);
+
+             Polynomial result = first - second;
+             Assert.AreEqual(0, result.ResultOfPolynomial(1));
+         }
+         [TestMethod]
+         public void Reduce_OfPolynomialAndNumber()
+         {
+             Polynomial first = new Polynomial(5, 7);
+             Polynomial result = 5 - first;
+
+             Assert.AreEqual(7, result.ResultOfPolynomial(1));
+         }
+         [TestMethod]
+         public void Multiply_OfPolynomialAndNumber()
+         {
+             Polynomial first = new Polynomial(5, 7);
+             Polynomial result = 3 * first;
+
+             Assert.AreEqual(36, result.ResultOfPolynomial(1));
+         }
+         [TestMethod]
+         [ExpectedException(typeof(ArgumentNullException))]
+         public void Multiply_OfPolynomialAndNUll()
+         {
+             Polynomial first = null;
+
+             Polynomial result = 5 * first;
+             Assert.AreEqual(7, result.ResultOfPolynomial(1));
+         }
+        [TestMethod]
+        public void GetHashCodeOfTwoSamePolynomials()
+         {
+             Polynomial first = new Polynomial( 4, 8);
+             Polynomial second = first.Clone();
+
+             Assert.AreEqual(first.GetHashCode(), second.GetHashCode());
+         }
+         [TestMethod]
+        public void GetHashCodeOfTwoDifferentPolynomials()
+        {
+            Polynomial first = new Polynomial(4, 8);
+            Polynomial second = new Polynomial();
+
+            Assert.AreNotEqual(first.GetHashCode(), second.GetHashCode());
+        }
+    }
+}
